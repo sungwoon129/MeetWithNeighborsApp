81f329a379a1d8086ca7d84081d19e173f353179
master <sungwoony@kakao.com>
2023-06-03 01:59:41 +0900

    Initial commit

---
+++ src/main/java/io/weyoui/meetwithneighborsapp/MeetWithNeighborsAppApplication.java
@@ -0,0 +1,13 @@
+package io.weyoui.meetwithneighborsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetWithNeighborsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetWithNeighborsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/weyoui/meetwithneighborsapp/MeetWithNeighborsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.weyoui.meetwithneighborsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetWithNeighborsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
