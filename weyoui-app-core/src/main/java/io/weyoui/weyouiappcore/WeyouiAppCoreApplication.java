d9831d398d6f73404b4462b324b2e31b79f730ff
master <sungwoony@kakao.com>
2023-06-08 02:48:02 +0900

    build. stage에 누락된 테스트 코드 추가 및 dev 프로필 git ignore 추가

---
+++ weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/WeyouiAppCoreApplication.java
@@ -0,0 +1,13 @@
+package io.weyoui.weyouiappcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeyouiAppCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeyouiAppCoreApplication.class, args);
+    }
+
+}
---
+++ weyoui-app-core/src/test/java/io/weyoui/weyouiappcore/WeyouiAppCoreApplicationTests.java
@@ -0,0 +1,14 @@
+package io.weyoui.weyouiappcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+class WeyouiAppCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
