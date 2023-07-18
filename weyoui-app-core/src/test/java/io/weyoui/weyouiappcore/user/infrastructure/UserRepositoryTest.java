7b992a0f165be5f92a6db98fc69b15149b29b2eb
master <sungwoony@kakao.com>
2023-07-19 03:10:21 +0900

    Test. 회원가입 관련 테스트코드 추가
    - 비밀번호 암호화 테스트
    - 유효성 검사 테스트
    - 이메일중복 검사 테스트
    - 비밀번호, 비밀번호 확인 값 비교 테스트

---
+++ weyoui-app-core/src/test/java/io/weyoui/weyouiappcore/user/infrastructure/UserRepositoryTest.java
@@ -0,0 +1,51 @@
+package io.weyoui.weyouiappcore.user.infrastructure;
+
+import io.weyoui.weyouiappcore.user.domain.RoleType;
+import io.weyoui.weyouiappcore.user.domain.User;
+import io.weyoui.weyouiappcore.user.presentation.dto.request.SignUpRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @DisplayName("회원 가입할 때 입력한 비밀번호가 암호화되어서 DB에 저장된다")
+    @Test
+    void encodePassword_success() {
+        //given
+        SignUpRequest signUpRequest = SignUpRequest.builder()
+                .email("example@weyoui.com")
+                .password("123456")
+                .passwordConfirm("123456")
+                .build();
+
+        //when
+        User user = User.builder()
+                .id(userRepository.nextUserId())
+                .password(new BCryptPasswordEncoder().encode(signUpRequest.getPassword()))
+                .role(RoleType.USER)
+                .build();
+
+        userRepository.save(user);
+
+        Page<User> users = userRepository.findAll(PageRequest.of(0,20));
+        User findUser = users.stream().findFirst().orElseThrow(() -> new NullPointerException("불러올 회원이 존재하지 않습니다."));
+
+        //then
+        assertThat(signUpRequest.getPassword()).isNotEqualTo(findUser.getPassword());
+
+    }
+
+}
--- weyoui-app-core/src/test/java/io/weyoui/weyouiappcore/user/presentation/GuestControllerTest.java
+++ weyoui-app-core/src/test/java/io/weyoui/weyouiappcore/user/presentation/GuestControllerTest.java
@@ -1,29 +1,41 @@
 package io.weyoui.weyouiappcore.user.presentation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.weyoui.weyouiappcore.user.application.UserService;
 import io.weyoui.weyouiappcore.user.presentation.dto.request.SignUpRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest
+@ExtendWith(SpringExtension.class)
 class GuestControllerTest {
 
     @Autowired
     MockMvc mvc;
 
+    @MockBean
+    UserService userService;
+
     ObjectMapper objectMapper = new ObjectMapper();
 
+
     @DisplayName("ID와 비밀번호가 유효성검사를 통과해야 회원가입이 가능하다")
+    @WithMockUser(username = "test@weyoui.com", roles = {"USER"})
     @Test
     void signUpValidate() throws Exception {
         //given
@@ -38,10 +50,10 @@ class GuestControllerTest {
                 post(apiUrl)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
+                .with(csrf())
                 )
             .andDo(print())
             .andExpect(status().isOk());
     }
-
 
 }
