a6d8c1c2d236726199283d568cd90484e3d9a48a
master <sungwoony@kakao.com>
2023-07-19 03:08:06 +0900

    Fix. UserId 클래스의 id 필드값을 얻어올 수 없던 버그 수정

--- weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/user/domain/UserId.java
+++ weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/user/domain/UserId.java
@@ -2,10 +2,12 @@ package io.weyoui.weyouiappcore.user.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+@Getter
 @Embeddable
 public class UserId implements Serializable {
 
