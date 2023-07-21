c2cb276f0508977a1c5ea741cb846076df72ac93
master <sungwoony@kakao.com>
2023-07-22 03:16:28 +0900

    Fix. login api는 인증받지 못한 유저도 접근할 수 있도록 변경

--- weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/user/presentation/GuestController.java
+++ weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/user/presentation/GuestController.java
@@ -1,9 +1,11 @@
 package io.weyoui.weyouiappcore.user.presentation;
 
-import io.weyoui.domain.CommonResponse;
+import io.weyoui.weyouiappcore.common.CommonResponse;
 import io.weyoui.weyouiappcore.user.application.UserService;
 import io.weyoui.weyouiappcore.user.domain.UserId;
+import io.weyoui.weyouiappcore.user.presentation.dto.request.LoginRequest;
 import io.weyoui.weyouiappcore.user.presentation.dto.request.SignUpRequest;
+import io.weyoui.weyouiappcore.user.presentation.dto.response.UserResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class GuestController {
 
     private final UserService userService;
+
+    @PostMapping("/api/v1/guest/login")
+    public ResponseEntity<CommonResponse<UserResponse.Token>> login(@RequestBody LoginRequest loginRequest) {
+
+        UserResponse.Token token = userService.login(loginRequest);
+
+        return ResponseEntity.ok(new CommonResponse<>(token));
+    }
 
     @PostMapping("/api/v1/guest/sign-up")
     public ResponseEntity<CommonResponse<UserId>> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
