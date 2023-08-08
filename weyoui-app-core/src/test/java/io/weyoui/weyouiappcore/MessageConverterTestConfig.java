889b8d0572d06f73f85cda62c8d1429dc1462416
master <sungwoony@kakao.com>
2023-08-09 02:52:44 +0900

    test. mockMvc http 응답메시지의 인코딩방식이 기본적으로 UTF8을 사용하도록 변경

---
+++ weyoui-app-core/src/test/java/io/weyoui/weyouiappcore/MessageConverterTestConfig.java
@@ -0,0 +1,22 @@
+package io.weyoui.weyouiappcore;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@TestConfiguration
+public class MessageConverterTestConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.stream()
+                .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
+                .findFirst()
+                .ifPresent(converter -> ((MappingJackson2HttpMessageConverter) converter).setDefaultCharset(UTF_8));
+    }
+}
