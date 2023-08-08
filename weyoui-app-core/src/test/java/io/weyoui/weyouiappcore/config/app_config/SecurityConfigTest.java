ce9c336dc6cd08cc50962d5259b7f54ca6c535a5
master <sungwoony@kakao.com>
2023-08-09 02:50:40 +0900

    fix: 사용자 권한 테스트코드 정상적으로 동작하도록 수정

--- weyoui-app-core/src/test/java/io/weyoui/weyouiappcore/config/app_config/SecurityConfigTest.java
+++ weyoui-app-core/src/test/java/io/weyoui/weyouiappcore/config/app_config/SecurityConfigTest.java
@@ -7,17 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -41,7 +37,7 @@ class SecurityConfigTest {
 
     }
 
-    @WithMockUser(roles = "{USER}")
+    @WithMockUser
     @DisplayName("인가받지 못한 사용자는 권한이 없는 자원에 접근할 수 없다.")
     @Test
     void testAuthorize() throws Exception {
