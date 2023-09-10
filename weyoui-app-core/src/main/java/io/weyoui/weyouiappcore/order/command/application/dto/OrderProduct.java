6e0d53d9c1414ebc97ee2d6e09a448790b7669f7
master <sungwoony@kakao.com>
2023-09-10 15:54:34 +0900

    fix. 주문 요청 dto에 상품의 이름 필드 추가(필수값 X)
    - 추후 DB 상품이름과 비교하는 로직 추가예정

--- weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/order/command/application/dto/OrderProduct.java
+++ weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/order/command/application/dto/OrderProduct.java
@@ -2,16 +2,19 @@ package io.weyoui.weyouiappcore.order.command.application.dto;
 
 import io.weyoui.weyouiappcore.common.model.Money;
 import io.weyoui.weyouiappcore.product.command.domain.ProductId;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class OrderProduct {
 
     private ProductId productId;
+    private String name;
     private Money price;
     private int quantity;
 
