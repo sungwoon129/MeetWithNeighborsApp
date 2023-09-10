0e544e457bf66da7fbd2c8b26cf18764b5fe1837
master <sungwoony@kakao.com>
2023-09-10 15:53:36 +0900

    fix. 테스트 SQL에 주문 테스트에 필요한 sql문 추가

--- weyoui-app-core/src/test/resources/store-init-test.sql
+++ weyoui-app-core/src/test/resources/store-init-test.sql
@@ -11,8 +11,14 @@ truncate table orders;
 
 set FOREIGN_KEY_CHECKS = 1;
 
-insert into users(user_id,nickname,email, user_state) values('user1','임의의 회원1', 'anyuser1@test.com', 'ACTIVE');
-insert into users(user_id,nickname, email, user_state) values('user2', '임의의 회원2', 'anyuser2@test.com', 'ACTIVE');
+insert into users(user_id,nickname,email, user_state, device_id, device_type, phone) values('user1','임의의 회원1', 'anyuser1@test.com', 'ACTIVE', 'device1', 'ios', '01012345678');
+insert into users(user_id,nickname, email, user_state, device_id, device_type, phone) values('user2', '임의의 회원2', 'anyuser2@test.com', 'ACTIVE', 'device2', 'android', '01087654321');
+
+insert into groups(group_id, group_name, state, point)
+values('group1', '임의의 모임', "END_ACTIVITY", ST_GeomFromText('POINT (37.37720712440026 127.11215415764482)'));
+
+insert into group_member(group_member_id, group_id, user_id, state, role)
+values('groupMember1', 'group1', 'user1', 'ACTIVE', 'LEADER');
 
 insert into store(store_id,store_name,category,state,owner_id,owner_name,zip_code,address1,address2,point)
 values('store1','임의의 가게','SERVICE','OPEN','user1','임의의 회원1','123-456','경기도','성남시',ST_GeomFromText('POINT (37.37720712440026 127.11215415764482)'));
