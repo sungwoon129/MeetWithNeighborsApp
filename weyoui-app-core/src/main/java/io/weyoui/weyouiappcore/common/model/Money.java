04d44c4e848bc65f2985862a428a58b5e8b8e6ff
master <sungwoony@kakao.com>
2023-09-11 19:27:20 +0900

    fix. 불필요 equals, hascode 재정의 함수 제거

--- weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/common/model/Money.java
+++ weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/common/model/Money.java
@@ -28,16 +28,4 @@ public class Money {
         return Integer.toString(value);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Money money = (Money) o;
-        return value == money.value;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
 }
