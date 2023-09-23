e1d169b1065a6aecadf1127983c7936ef3cf2f0a
master <sungwoony@kakao.com>
2023-09-23 18:08:44 +0900

    style. 불필요한 import문 제거

--- weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/config/swagger/SwaggerConfig.java
+++ weyoui-app-core/src/main/java/io/weyoui/weyouiappcore/config/swagger/SwaggerConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.models.OpenAPI;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
